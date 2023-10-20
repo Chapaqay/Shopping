300b678641b02449b7f4cdd65ddaea73f0f42caf
Chapaqay <sardorbek.olimjonovich>
2023-10-20 09:53:26 +0500

    Card UI

---
+++ app/src/main/java/com/example/shopping/Activity/CartActivity.java
@@ -0,0 +1,16 @@
+package com.example.shopping.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.shopping.R;
+
+public class CartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
