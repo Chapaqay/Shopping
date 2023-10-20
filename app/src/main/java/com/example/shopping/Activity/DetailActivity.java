d33693885d902d2c5828e151c82adb8af292321a
Chapaqay <sardorbek.olimjonovich>
2023-10-20 08:31:38 +0500

    update

---
+++ app/src/main/java/com/example/shopping/Activity/DetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.shopping.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.shopping.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
